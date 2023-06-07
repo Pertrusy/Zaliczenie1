33acde80a65dad73ec5f36d5dfcd3c66796066f3
Pertrusy <127326311+Pertrusy@users.noreply.github.com>
2023-06-07 13:36:54 +0000

    Zaliczenie1

--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj rozmiar tablicy: ");
+        int rozmiar = scanner.nextInt();
+
+        int[] tablica = new int[rozmiar];
+
+        System.out.println("Podaj elementy tablicy:");
+        for (int i = 0; i < rozmiar; i++) {
+            System.out.print("Element " + (i + 1) + ": ");
+            tablica[i] = scanner.nextInt();
+        }
+      int iloczyn = 1;
+
+        for (int i = 0; i < rozmiar; i++) {
+            iloczyn *= tablica[i];
+        }
+
+        System.out.println("Iloczyn elementów tablicy: " + iloczyn);
+    }
 }
